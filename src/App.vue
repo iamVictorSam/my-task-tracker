c030d871ced81b42ca258467f5422a5c0edda4a1
IamVictorSam <iamvictorsam@gmail.com>
2022-08-27 22:43:06 +0100

    final

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,105 @@
 <template>
-//jsx here
+
+  <main class="app">
+    <section class="greeting">
+      <h3 class="title">😊MY TASK TRACKER</h3>
+    </section> 
+
+    <section class="create-task">
+      <form @submit.prevent="addTask">
+        <h3>What's on your Mind</h3>
+        <input type="text"
+               placeholder="e.g. email your boss"
+               v-model="input_text"/>
+
+        <h4>Choose a Category</h4>
+
+        <div class="cate-option">
+          <label>
+            <input type="radio"
+                   name="category"
+                   value="business"
+                   v-model="input_category"/>
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input type="radio"
+                   name="category"
+                   value="personal"
+                   v-model="input_category"/>
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+
+        <input type="submit" value="Add Task"/>
+      </form>
+    </section>
+
+    <section class="task-list">
+      <h3>TASK LIST</h3>
+      <div class="list">
+
+        <div v-for="task in tasks" :class="`task-item ${task.completed && 'done'}`">
+          <label >
+            <input type="checkbox" v-model="task.completed"/>
+            <span :class="`bubble ${task.category}`"></span>
+          </label>
+
+          <div class="task-content">
+            <input type="text" v-model="task.task">
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTask(task)">Delete</button>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main>
 
 </template>
 
+
 <script setup>
-// Javascript code here
+import {ref, onMounted, watch} from "vue";
+
+const tasks = ref([]);
+const input_text = ref('');
+const input_category = ref(null);
+
+
+const addTask = () => {
+  if (input_text.value.trim() === '' || input_category === null) {
+    return
+  }
+
+  tasks.value.unshift({
+    task: input_text.value,
+    category: input_category.value,
+    completed: false,
+  })
+
+  input_text.value = '';
+  input_category.value = null;
+}
+
+
+
+watch(tasks, (newValue) => {
+  localStorage.setItem("tasks", JSON.stringify(newValue));
+}, {deep: true});
+
+const removeTask =(task) =>{
+  tasks.value = tasks.value.filter((x)=> x !== task)
+}
+
+onMounted(() => {
+  tasks.value = JSON.parse(localStorage.getItem('tasks')) || [];
+});
 
 </script>
-
